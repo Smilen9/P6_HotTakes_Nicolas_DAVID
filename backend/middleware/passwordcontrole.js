ec4b62cd2b19b1830e76da598fec87521e6ae732
Nicolas David <nicolasdavid9.nd@gmail.com>
2023-02-08 16:04:53 +0100

    modif mots de passe schema

--- backend/middleware/passwordcontrole.js
+++ backend/middleware/passwordcontrole.js
@@ -1,10 +1,23 @@
-const passwordSchema = require('../models/password');
+// mise en place de password validator
+var passwordValidator = require('password-validator');
 
-//----Middleware de la verification du MDP
+// Création du schéma
+var schemaPassword = new passwordValidator();
+
+// shéma pour mot de passe
+schemaPassword
+.is().min(7)                                    // nombre de caractere minimum
+.is().max(25)                                  // nombre de caractere maximum
+.has().uppercase()                              // majuscules obligatoire
+.has().lowercase()                              // minuscules obligatoire
+.has().digits(1)                                // minimum 2 chiffres
+.has().not().spaces()                           // espaces interdits
+.is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist de mot de passe
+
 module.exports = (req, res, next) => {
-    if (!passwordSchema.validate(req.body.password)) {
-        res.status(400).json({ message: 'Le MDP doit faire 7 caractères minimum, avec une maj, une min et un chiffre au minimum.' });
-    } else {
-        next();
+    if(schemaPassword.validate(req.body.password)){
+        return next();
+    }else{
+        return res.status(400).json({error: `mot de passe invalide ${schemaPassword.validate('req.body.password', { list: true })}` })
     }
-};
+}
