2ade0e8f560e22173aebfc15c4462dac0cb1010d
Nicolas David <nicolasdavid9.nd@gmail.com>
2023-02-08 16:14:53 +0100

    fix bug nombre mot de passe

--- backend/middleware/passwordcontrole.js
+++ backend/middleware/passwordcontrole.js
@@ -7,10 +7,10 @@ var schemaPassword = new passwordValidator();
 // shéma pour mot de passe
 schemaPassword
 .is().min(7)                                    // nombre de caractere minimum
-.is().max(25)                                  // nombre de caractere maximum
+.is().max(25)                                   // nombre de caractere maximum
 .has().uppercase()                              // majuscules obligatoire
 .has().lowercase()                              // minuscules obligatoire
-.has().digits(1)                                // minimum 2 chiffres
+.has().digits()                                 // minimum 1 chiffres
 .has().not().spaces()                           // espaces interdits
 .is().not().oneOf(['Passw0rd', 'Password123']); // Blacklist de mot de passe
 
