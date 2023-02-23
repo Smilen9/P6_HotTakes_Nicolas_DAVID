a095c534dc27c4124b1530b64bf7d9a252d7bfd6
Nicolas David <nicolasdavid9.nd@gmail.com>
2023-02-23 11:55:22 +0100

    Ajout de commentaires + plugin 'ALERT'

--- backend/middleware/emailcontrole.js
+++ backend/middleware/emailcontrole.js
@@ -1,10 +1,11 @@
 //-------------- Regex du mail
+const alert = require ('alert');
 
 module.exports = (req, res, next) => {
     const validEmail = (email) => {
-        let emailRegexp =  /^[_Aa-Zz0-9-]+(.[_Aa-Zz0-9-]+)*@[Aa-Zz0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/
+        let emailRegexp = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/
         let isRegexTrue = emailRegexp.test(email)
-        isRegexTrue ? next() : res.status(400).json({ message: 'mail non valide' }); // ? = IF et : = ELSE
+        isRegexTrue ? next() : res.status(400).json(alert('mail non valide')); // ? = IF et : = ELSE
     }
     validEmail(req.body.email)
   };
